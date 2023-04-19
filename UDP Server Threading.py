eae84f78ef0b6295e22cfdb6cc8beac609243588
masagi94 <32596245+masagi94@users.noreply.github.com>
2023-04-19 13:30:31 -0400

    added graceful quitting of Server

--- UDP Server Threading.py
+++ UDP Server Threading.py
@@ -5,14 +5,15 @@ import torch
 import torchvision
 import threading
 import queue
-import pathlib
 
 torch.set_num_threads(20)
 
 # Threaded method used to listen for incomming frames sent by the client
 def receive_frame():
-    
-    while True:
+    print("\n**********************************************************")
+    print("\nReady to receive frames, listening from Receiver Thread...\n")
+
+    while (user_input != 'q'):
         # Receive a UDP packet containing a JPEG image
         # print("Listening on port ", port)
         data, addr = sock.recvfrom(65535)
@@ -22,6 +23,9 @@ def receive_frame():
         # add frame to queue for processing
         frames_queue.put((data, addr))
 
+    
+    print("\nExiting Receiver Thread...\n")
+    return()
 
 
 # Threaded method used for processing and sending frames with bounding boxes to client
@@ -30,7 +34,7 @@ def send_predictions():
     while True:
         
         # Processes a frame once the queue is no longer empty
-        while not frames_queue.empty():
+        while (not frames_queue.empty()) and (user_input != 'q'):
             data, addr =  frames_queue.get()
             
             # read frame
@@ -59,6 +63,10 @@ def send_predictions():
             except:
                 print("******************************* ERROR - FRAME NOT SENT *******************************")
 
+        if (user_input == 'q'):
+            print("\nExiting Predictions Thread...\n")
+            return()
+
 
 
 # Create the queue that will hold frames as they come in
@@ -70,7 +78,7 @@ model = torch.hub.load('Ultralytics/yolov5', 'yolov5x')
 # only send detections above .70 confidence
 model.conf = 0.70
 
-encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), 70]
 
 # IP address of pc where server is running
 ip = '192.168.1.189'
@@ -84,6 +92,26 @@ sock.bind((ip, port))
 t1 = threading.Thread(target=receive_frame)
 t2 = threading.Thread(target=send_predictions)
 
+user_input = ""
+
 # start threads
 t1.start()
 t2.start()
+
+# check user input to exit program
+while True:
+    user_input = input("\nType 'q' to quit\n")
+    if(user_input == 'q'):
+        print("\n\n***********************")
+        print("Shutting down Server...")
+        
+        # send message to receiver thread to unblock it
+        sock.sendto(bytes("exit", 'utf-8'), (ip,port))
+        t1.join()
+        t2.join()
+        print("\n\n***********************")
+        print("Threads quit successfully")
+        exit()
+    else:
+        print("non valid input")
+
