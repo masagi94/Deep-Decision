4adaa48ce788e808fada63cd167d0819e3e62069
masagi94 <32596245+masagi94@users.noreply.github.com>
2023-04-25 11:43:25 -0400

    Client and Server now work together automatically with rudimentary DeepDecision Framework

--- UDP Server Threading.py
+++ UDP Server Threading.py
@@ -1,46 +1,41 @@
 import cv2
 import numpy as np
 import socket
+import time
 import torch
 import torchvision
 import threading
 import queue
+from multiprocessing import Manager
 
-torch.set_num_threads(20)
+# torch.set_num_threads(20)
 
-# Threaded method used to listen for incomming frames sent by the client
+# Threaded method used to listen for incomming UDP frames sent by the client.
+# Add received JPEG images to queue for processing.
 def receive_frame():
     print("\n**********************************************************")
-    print("\nReady to receive frames, listening from Receiver Thread...\n")
+    print("\nReady to receive frames...\n")
+    print("\nListening on:",(ip, port),"\n")
 
     while (user_input != 'q'):
-        # Receive a UDP packet containing a JPEG image
-        # print("Listening on port ", port)
         data, addr = sock.recvfrom(65535)
-        
-        # print("FRAME RECEIVED")
-        
-        # add frame to queue for processing
-        frames_queue.put((data, addr))
+        # time.sleep(send_latency/1000)
+        frames_process_queue.put((data, addr))
 
-    
     print("\nExiting Receiver Thread...\n")
     return()
 
 
 # Threaded method used for processing and sending frames with bounding boxes to client
-def send_predictions():
-    
+def process_frame():
     while True:
         
         # Processes a frame once the queue is no longer empty
-        while (not frames_queue.empty()) and (user_input != 'q'):
-            data, addr =  frames_queue.get()
+        while (not frames_process_queue.empty()) and (user_input != 'q'):
+            # get frame and decode
             
-            # read frame
-            img_encoded = np.frombuffer(data, dtype=np.uint8)
-
-            # Decode the JPEG frame
+            data, addr =  frames_process_queue.get()
+            img_encoded = np.frombuffer(data, dtype=np.uint8) 
             img = cv2.imdecode(img_encoded, cv2.IMREAD_COLOR)
             
             # Get predictions on frame from model
@@ -49,69 +44,95 @@ def send_predictions():
             # Render the predicted bounding boxes on the frame
             r_img = results.render()
             img_with_boxes = r_img[0]
-            
+
             # Encode frame to send to client
             _, new_img_encoded = cv2.imencode('.jpg', img_with_boxes[:,:,::-1], encode_params)
             data = np.array(new_img_encoded)
             string_data = data.tobytes()
             
-            # Try to send frame to client
-            # Sometimes fails if frame size is too big
+            predictions_send_queue.put((string_data, addr))
+            # print("FRAME PROCESSED")
+            # try:
+            #     # time.sleep(send_latency/1000)
+            #     sock.sendto(string_data, addr)
+            #     # predictions_send_queue.put((string_data, addr))
+            # except:
+            #     print("******************************* ERROR - FRAME NOT SENT *******************************")
+
+        if (user_input == 'q'):
+            print("\nExiting Processing Thread...\n")
+            return()
+
+def send_predictions():
+    while True:
+        while((not predictions_send_queue.empty()) and (user_input != 'q')):
+            data, addr =  predictions_send_queue.get()
             try:
-                sock.sendto(string_data, addr)
-                # print("FRAME SENT")
+                sock.sendto(data, addr)
+                # print("FRAME SENT. LATENCY:", send_latency)
             except:
                 print("******************************* ERROR - FRAME NOT SENT *******************************")
 
+
+            time.sleep(send_latency/1000)
+            try:
+                while True:
+                    predictions_send_queue.get_nowait()
+            except:
+                pass
         if (user_input == 'q'):
             print("\nExiting Predictions Thread...\n")
             return()
 
+if __name__ == '__main__':
+    manager1 = Manager()
+    # inq = manager1.Queue()
 
+    # Create the queue that will hold frames as they come in
+    frames_process_queue = manager1.Queue()
+    predictions_send_queue = manager1.Queue()
+    # frames_process_queue = queue.Queue()
+    # predictions_send_queue = queue.Queue()
 
-# Create the queue that will hold frames as they come in
-frames_queue = queue.Queue()
+    # Import YOLO Model
+    model = torch.hub.load('Ultralytics/yolov5', 'yolov5x', trust_repo=True, force_reload=True).to("cuda")
+    model.conf = 0.70
 
-# Import YOLO Model
-model = torch.hub.load('Ultralytics/yolov5', 'yolov5x')
+    encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), 70]
 
-# only send detections above .70 confidence
-model.conf = 0.70
+    # Create a UDP socket and bind it to the specified IP address and port of the server
+    ip = '192.168.1.189'
+    port = 5005
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port))
 
-encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), 70]
+    # Create worker threads
+    t1 = threading.Thread(target=receive_frame)
+    t2 = threading.Thread(target=process_frame)
+    t3 = threading.Thread(target=send_predictions)
+    user_input = ""
+    send_latency = 3
 
-# IP address of pc where server is running
-ip = '192.168.1.189'
-port = 5005
+    t1.start()
+    t2.start()
+    t3.start()
 
-# Create a UDP socket and bind it to the specified IP address and port
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((ip, port))
+    # check user input to exit program
+    while True:
+        user_input = input("\nType in new latency (ms), or 'q' to quit\n")
 
-# Create worker threads
-t1 = threading.Thread(target=receive_frame)
-t2 = threading.Thread(target=send_predictions)
-
-user_input = ""
-
-# start threads
-t1.start()
-t2.start()
-
-# check user input to exit program
-while True:
-    user_input = input("\nType 'q' to quit\n")
-    if(user_input == 'q'):
-        print("\n\n***********************")
-        print("Shutting down Server...")
-        
-        # send message to receiver thread to unblock it
-        sock.sendto(bytes("exit", 'utf-8'), (ip,port))
-        t1.join()
-        t2.join()
-        print("\n\n***********************")
-        print("Threads quit successfully")
-        exit()
-    else:
-        print("non valid input")
-
+        if(user_input == 'q'):
+            print("\n\n***********************")
+            print("Shutting down Server...")
+            
+            # send message to receiver thread to unblock it
+            sock.sendto(bytes("exit", 'utf-8'), (ip,port))
+            t1.join()
+            t2.join()
+            t3.join()
+            print("\n\n***********************")
+            print("Threads quit successfully")
+            exit()
+        elif(user_input.isdigit()):
+            send_latency = int(user_input)
+            print("\nLatency updated to: " + str(user_input) + " ms\n")
