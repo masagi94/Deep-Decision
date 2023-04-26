7b08d9a0f40957390eb8241bd051bae6961abfb1
masagi94 <32596245+masagi94@users.noreply.github.com>
2023-04-26 13:57:38 -0400

    added prettier printouts, and ability to invert images based on selected client (phone or laptop)

--- UDP Server Threading.py
+++ UDP Server Threading.py
@@ -5,7 +5,6 @@ import time
 import torch
 import torchvision
 import threading
-import queue
 from multiprocessing import Manager
 
 # torch.set_num_threads(20)
@@ -19,14 +18,14 @@ def receive_frame():
 
     while (user_input != 'q'):
         data, addr = sock.recvfrom(65535)
-        # time.sleep(send_latency/1000)
         frames_process_queue.put((data, addr))
 
     print("\nExiting Receiver Thread...\n")
     return()
 
 
-# Threaded method used for processing and sending frames with bounding boxes to client
+# Threaded method used for processing frames and drawing bounding boxes. Adds them
+# to sending queue
 def process_frame():
     while True:
         
@@ -38,6 +37,10 @@ def process_frame():
             img_encoded = np.frombuffer(data, dtype=np.uint8) 
             img = cv2.imdecode(img_encoded, cv2.IMREAD_COLOR)
             
+            # invert image if we're using phone or laptop as client
+            if(invert_image):
+                img = img[:,:,::-1]
+
             # Get predictions on frame from model
             results = model(img)
             
@@ -51,48 +54,45 @@ def process_frame():
             string_data = data.tobytes()
             
             predictions_send_queue.put((string_data, addr))
-            # print("FRAME PROCESSED")
-            # try:
-            #     # time.sleep(send_latency/1000)
-            #     sock.sendto(string_data, addr)
-            #     # predictions_send_queue.put((string_data, addr))
-            # except:
-            #     print("******************************* ERROR - FRAME NOT SENT *******************************")
 
         if (user_input == 'q'):
             print("\nExiting Processing Thread...\n")
             return()
 
+# Threaded method for sending predictions back to client.
+# Pops predictions from the send queue.
 def send_predictions():
     while True:
         while((not predictions_send_queue.empty()) and (user_input != 'q')):
+            
+            # pop prediction and send it
             data, addr =  predictions_send_queue.get()
+            
             try:
                 sock.sendto(data, addr)
-                # print("FRAME SENT. LATENCY:", send_latency)
             except:
                 print("******************************* ERROR - FRAME NOT SENT *******************************")
 
-
+            # delay packets, latency specified by user
             time.sleep(send_latency/1000)
+            
+            # clear queue, otherwise the delay stacks
             try:
                 while True:
                     predictions_send_queue.get_nowait()
             except:
                 pass
+        
         if (user_input == 'q'):
-            print("\nExiting Predictions Thread...\n")
+            print("\nExiting Sending Thread...\n")
             return()
 
 if __name__ == '__main__':
     manager1 = Manager()
-    # inq = manager1.Queue()
 
     # Create the queue that will hold frames as they come in
     frames_process_queue = manager1.Queue()
     predictions_send_queue = manager1.Queue()
-    # frames_process_queue = queue.Queue()
-    # predictions_send_queue = queue.Queue()
 
     # Import YOLO Model
     model = torch.hub.load('Ultralytics/yolov5', 'yolov5x', trust_repo=True, force_reload=True).to("cuda")
@@ -111,15 +111,22 @@ if __name__ == '__main__':
     t2 = threading.Thread(target=process_frame)
     t3 = threading.Thread(target=send_predictions)
     user_input = ""
-    send_latency = 3
-
+    send_latency = 0
+    invert_image = False
     t1.start()
     t2.start()
     t3.start()
 
-    # check user input to exit program
+    # check user input to update server functionality, or exit program
     while True:
-        user_input = input("\nType in new latency (ms), or 'q' to quit\n")
+        user_input = input("\n\n\n--User options--\n" + 
+                            "   Type:\n"+
+                            "       'q'          -- shuts down the server\n"+
+                            "       int (0-1000) -- adds latency (ms)\n" +
+                            "       'phone'      -- sets client as phone\n"+
+                            "       'laptop'     -- sets client as laptop\n\n"+
+                            "   Input: ")
+
 
         if(user_input == 'q'):
             print("\n\n***********************")
@@ -133,6 +140,18 @@ if __name__ == '__main__':
             print("\n\n***********************")
             print("Threads quit successfully")
             exit()
+
+        elif(user_input == 'phone'):
+            print("\nPhone Client Selected")
+            invert_image = True
+
+        elif(user_input == 'laptop'):
+            print("\nLaptop Client Selected")
+            invert_image = False
+
         elif(user_input.isdigit()):
             send_latency = int(user_input)
             print("\nLatency updated to: " + str(user_input) + " ms\n")
+
+        else:
+            print("\n*** INVALID INPUT ***\n")
